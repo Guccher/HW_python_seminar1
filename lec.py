3762ad68f83cd0cb828bcdaac289109a8edc8eb0
Guccher <Guccher_007@mail.ru>
2022-09-27 18:16:42 +0300

    did my python HW #1

---
+++ lec.py
@@ -0,0 +1,63 @@
+# Task № 1
+
+# mass = int(input("Enter week day: "))
+# if (mass == 6) or (mass == 7):
+#     print("Yes")
+# else:
+#     print("No")
+
+
+# Task № 2
+
+# for X in range(2):
+#     for Y in range(2):
+#         for Z in range(2):
+#             print(f'{X, Y, Z} : {(not (X or Y or Z)) == (not X and not Y and not Z)}')
+
+
+# Task №3
+
+# X = int(input('Введите координату X: '))
+# Y = int(input('Введите координату Y: '))
+# if (X == 0 or Y == 0):
+#     print(f'{X,Y} : введённые данные не соответствуют условию')
+# elif (X > 0 and Y > 0):
+#     print(f'{X,Y} : координаты плоскости находятся в {1} четверти')
+# elif (X < 0 and Y > 0):
+#     print(f'{X,Y} : координаты плоскости находятся в {2} четверти')
+# elif (X < 0 and Y < 0):
+#     print(f'{X,Y} : координаты плоскости находятся в {3} четверти')
+# elif (X > 0 and Y < 0):
+#     print(f'{X,Y} : координаты плоскости находятся в {4} четверти')
+
+
+# Task № 4
+
+# plane = int(input('Введите четверть плоскости: '))
+# if (plane == 1):
+#     for X in range(0, 10):
+#         for Y in range(0, 10):
+#             print(f'{X,Y}')
+# elif (plane == 2):
+#     for X in range(-10, 0):
+#         for Y in range(0, 10):
+#             print(f'{X,Y}')
+# elif (plane == 3):
+#     for X in range(-10, 0):
+#         for Y in range(-10, 0):
+#             print(f'{X,Y}')
+# elif (plane == 4):
+#     for X in range(0, 10):
+#         for Y in range(-10, 0):
+#             print(f'{X,Y}')
+
+
+# Task № 5
+import math
+# X1 = int(input('Введите координату X1: '))
+# Y1 = int(input('Введите координату Y1: '))
+# X2 = int(input('Введите координату X2: '))
+# Y2 = int(input('Введите координату Y2: '))
+# d = float (round((math.sqrt(math.pow(X1 - X2, 2) + math.pow(Y1 - Y2,2))),2))
+# print(d)
+
